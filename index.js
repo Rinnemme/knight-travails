5377b91cbdc578afd4f8d68b9d8cb0a028c6df2d
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-11-07 14:07:36 -0500

    clean up and organise javascript, remove functions related to alpha-izing space positions, the old shortestValid function left over from an earlier attempt, stringification of the result of shortestPath

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ class space {
     constructor(x,y) {
         this.x = x
         this.y = y
-        this.position = `${this.alpha(this.x)}-${this.y+1}`
         this.leftUp = null
         this.upLeft = null
         this.upRight = null
@@ -14,33 +13,11 @@ class space {
         this.moves = []
         this.previous = null
     }
-
-    alpha(x) {
-        if (x === 0) return 'A'
-        if (x === 1) return 'B'
-        if (x === 2) return 'C'
-        if (x === 3) return 'D'
-        if (x === 4) return 'E'
-        if (x === 5) return 'F'
-        if (x === 6) return 'G'
-        if (x === 7) return 'H'
-    }
 }
 
 const board = (() => {
+
     const spaces = []
-
-    const find = (x,y) => {
-        for (let i=0; i < 64; i++) {
-            if (spaces[i].x === x && spaces[i].y === y) return spaces[i]
-        }
-    }
-
-   const shortestValid = (arrayOfArrays, node) => {
-        const filteredArray = arrayOfArrays.filter(array => array.includes(node))
-        filteredArray.sort((a,b) => a.length > b.length ? + 1 : -1)
-        return filteredArray[0]
-   }
 
     for (let x = 0; x <= 7; x++) {
         for (let y = 7; y >= 0; y--) {
@@ -48,6 +25,13 @@ const board = (() => {
             spaces.push(newSpace)
         }
     }
+
+    const find = (x,y) => {
+        for (let i=0; i < 64; i++) {
+            if (spaces[i].x === x && spaces[i].y === y) return spaces[i]
+        }
+    }
+
     for (let i = 0; i < 64; i++) {
         const x = spaces[i].x
         const y = spaces[i].y
@@ -71,8 +55,7 @@ const board = (() => {
             if (!visited.includes(queue[0])) {
                 visited.push(queue[0])
                 queue[0].moves.forEach(move => {
-                    if (move !== null && !visited.includes(move)) {
-                        if(visited.includes(end)) return
+                    if (move !== null && !visited.includes(move) && !queue.includes(move)) {
                         move.previous = queue[0]
                         queue.push(move)
                     }
@@ -85,75 +68,15 @@ const board = (() => {
             path.unshift(path[0].previous)
         }
         return path
-        // let resultString = `You can get there in ${path.length-1} moves! The path to follow is [`
-        // path.forEach(space => {resultString += `${space.position} -> `})
-        // resultString = resultString.substring(0,resultString.length-4)
-        // resultString += ']'
-        // console.log(resultString)
-        // return resultString
     }
-    return {spaces, find, shortestPath}
+
+    return {find, shortestPath}
+    
 })()
 
 const display = (() => {
+
     const displayBoard = document.getElementById('board')
-
-    let knightStart = null
-    let knightEnd = null
-    
-    const setKnightStart = (element) => {
-        knightStart = board.find(+element.getAttribute('x'), +element.getAttribute('y'))
-        element.style.background = `url('img/start.png')`
-        element.style.backgroundSize = 'cover'
-    } 
-    
-    const setKnightEnd = (element) => {
-        knightEnd = board.find(+element.getAttribute('x'), +element.getAttribute('y'))
-        element.style.background = `url('img/end-${board.shortestPath(knightStart, knightEnd).length-1}.png')`
-        element.style.backgroundSize = 'cover'
-    }
-    
-    const resetStartAndEnd = () => {
-        resetBoardVisuals()
-        knightStart = null
-        knightEnd = null
-    }
-
-
-    const displaySpace = (x,y) => {
-        return document.getElementById(`${x}-${y}`)
-    }
-    
-    const highlightPath = () => {
-        pathArray = board.shortestPath(knightStart, knightEnd)
-        pathArray.pop()
-        pathArray.shift()
-        let step = 1
-        pathArray.forEach(space => {
-            const pathSpace = displaySpace(space.x, space.y)
-            pathSpace.style.background = `url('img/${step}.png')`
-            pathSpace.style.backgroundSize = 'cover'
-            step++
-        })
-    }
-    
-    const resetBoardVisuals = () => {
-        document.querySelectorAll('.space').forEach(space => {
-            space.style.background = space.getAttribute('default-color')
-        })
-    }
-
-    const setPath = (element) => {
-        if (knightStart === null) {
-            setKnightStart(element)
-        } else if (knightStart !== null && knightEnd === null) {
-            setKnightEnd(element)
-            highlightPath()
-        } else if (knightStart !== null && knightEnd !== null) {
-            resetStartAndEnd()
-            setKnightStart(element)
-        }
-    }
 
     let spaceColor = 'rgb(255,255,255)'
     for (let y = 7; y >= 0; y--) {
@@ -171,4 +94,61 @@ const display = (() => {
             if (x === 7) spaceColor = spaceColor === 'rgb(255,255,255)' ? 'rgb(30,30,30)' : 'rgb(255,255,255)'
         }
     }
+
+    let knightStart = null
+    let knightEnd = null
+    
+    const setKnightStart = (element) => {
+        knightStart = board.find(+element.getAttribute('x'), +element.getAttribute('y'))
+        element.style.background = `url('img/start.png')`
+        element.style.backgroundSize = 'cover'
+    } 
+    
+    const setKnightEnd = (element) => {
+        knightEnd = board.find(+element.getAttribute('x'), +element.getAttribute('y'))
+        element.style.background = `url('img/end-${board.shortestPath(knightStart, knightEnd).length-1}.png')`
+        element.style.backgroundSize = 'cover'
+    }
+
+    const displaySpace = (x,y) => {
+        return document.getElementById(`${x}-${y}`)
+    }
+    
+    const highlightPath = () => {
+        pathArray = board.shortestPath(knightStart, knightEnd)
+        pathArray.pop()
+        pathArray.shift()
+        let step = 1
+        pathArray.forEach(space => {
+            const pathSpace = displaySpace(space.x, space.y)
+            pathSpace.style.background = `url('img/${step}.png')`
+            pathSpace.style.backgroundSize = 'cover'
+            step++
+        })
+    }
+
+    const resetBoardVisuals = () => {
+        document.querySelectorAll('.space').forEach(space => {
+            space.style.background = space.getAttribute('default-color')
+        })
+    }
+
+    const resetStartAndEnd = () => {
+        knightStart = null
+        knightEnd = null
+    }
+
+    const setPath = (element) => {
+        if (knightStart === null) {
+            setKnightStart(element)
+        } else if (knightStart !== null && knightEnd === null) {
+            setKnightEnd(element)
+            highlightPath()
+        } else if (knightStart !== null && knightEnd !== null) {
+            resetStartAndEnd()
+            resetBoardVisuals()
+            setKnightStart(element)
+        }
+    }
+
 })()
